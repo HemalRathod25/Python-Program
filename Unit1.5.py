9d518cd56e50cc527c55344a690189c10bc02643
HemalRathod25 <hprathod047@gmail.com>
2024-08-29 18:45:19 +0530

    Create Unit1.5.py

---
+++ Unit1.5.py
@@ -0,0 +1,9 @@
+#Write a program to find out if the two blood groups match.
+
+BG1=input("Enter Blood Group 1 :").upper()
+BG2=input("Enter Blood Group 2 :").upper()
+
+if BG1==BG2:
+     print("Blood Groups Is Match :")
+else:
+    print("Blood Groups Is Not Match :")
