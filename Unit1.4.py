f2b0d5ced8704c4bede9be3abe21ea126afc7ea8
HemalRathod25 <hprathod047@gmail.com>
2024-08-29 18:29:14 +0530

    Create Unit1.4.py

---
+++ Unit1.4.py
@@ -0,0 +1,13 @@
+#Extracting fields of a roll number using string indexing and slicing.
+
+Name="Hello Python Programming"
+
+print("Index(0):     ",Name[0])
+print("Index(0:3):   ",Name[0:4])
+print("Index(6:11):  ",Name[2:-1])
+print("Index(-7:-1): ",Name[7:-1])
+print("Index(9):     ",Name[8])
+print("Index(0:-1):  ",Name[0:-1])
+print("Index(1:-3):  ",Name[1:-3])
+print("Index(7:10):  ",Name[7:10])
+print("Index(-7:-1): ",Name[-7:-3])
