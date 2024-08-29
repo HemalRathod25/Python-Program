b66d9f3ca035fa26074a3b4216a793465df68375
HemalRathod25 <hprathod047@gmail.com>
2024-08-29 17:56:00 +0530

    Update Unit1.3.py

--- Unit1.3.py
+++ Unit1.3.py
@@ -1,1 +1,13 @@
 #Extracting fields of a roll number using string indexing and slicing.
+
+String = "CS-MCA-22"
+#String = input("Enter String : \n");
+
+Department = String[:2]
+print(Department) 
+
+Course = String[3:6]
+print(Course)  
+
+Roll_no = String[7:]
+print(Roll_no) 
