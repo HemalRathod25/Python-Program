800376558bf8332b09f512ead69333d75db63556
HemalRathod25 <hprathod047@gmail.com>
2024-08-30 19:36:07 +0530

    Update Unit1.4.py

--- Unit1.4.py
+++ Unit1.4.py
@@ -3,11 +3,11 @@
 Name="Hello Python Programming"
 
 print("Index(0):     ",Name[0])
-print("Index(0:3):   ",Name[0:4])
-print("Index(6:11):  ",Name[2:-1])
-print("Index(-7:-1): ",Name[7:-1])
-print("Index(9):     ",Name[8])
+print("Index(0:4):   ",Name[0:4])
+print("Index(2:-1):  ",Name[2:-1])
+print("Index(7:-1): ",Name[7:-1])
+print("Index(8):     ",Name[8])
 print("Index(0:-1):  ",Name[0:-1])
 print("Index(1:-3):  ",Name[1:-3])
 print("Index(7:10):  ",Name[7:10])
-print("Index(-7:-1): ",Name[-7:-3])
+print("Index(-7:-3): ",Name[-7:-3])
